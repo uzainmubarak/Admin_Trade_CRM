58918c166c7242783b648e5ee52985e7c1e46c0d
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2025-01-16 20:25:57 +0500

    color push

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -46,8 +46,8 @@ export default function DashboardLayout({
               return (
                 <Link key={item.href} href={item.href}>
                   <span className={cn(
-                    "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50",
-                    pathname === item.href ? "bg-gray-100 text-gray-900" : ""
+                    "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all",
+                    pathname === item.href ? "bg-[#37bfb1] text-white" : ""
                   )}>
                     <Icon className="h-4 w-4" />
                     {item.name}
